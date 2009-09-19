22cd355e4ab03708d64d3e323e62561f43f38e4c
Hari K T <kthari85@gmail.com>
2009-09-20 00:25:08 +0530

    show albums

--- application/controllers/GalleryController.php
+++ application/controllers/GalleryController.php
@@ -1,19 +1,25 @@
 <?php
 
 class GalleryController extends Zend_Controller_Action
-{
-
+{	
 	public function init()
 	{
 		/* Initialize action controller here */
+		$this->user = 'kthari85';
 	}
 
 	public function indexAction()
 	{
 		// action body
+		/*
+		 * Get the various albums of current user .
+		 * Only public photos and albums will be shown
+		 * Not implemented the authentication
+		 * We can show when authenticated. But I am not adding for now .
+		 */
 		$service = new Zend_Gdata_Photos();
 		$query = new Zend_Gdata_Photos_UserQuery();
-		$query->setUser('kthari85');
+		$query->setUser($this->user);
 		try {
 			$userFeed = $service->getUserFeed(null, $query);
 			$this->view->userFeed = $userFeed;
@@ -25,6 +31,7 @@ class GalleryController extends Zend_Controller_Action
 	public function albumAction()
 	{
 		// action body
+		
 	}
 
 	public function viewAction()
